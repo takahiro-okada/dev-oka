c1c092ca133b28376054ba75dc413643705b06f5
takahiro-okada <okahanako09875@gmail.com>
2026-01-07 10:38:58 +0900

    ✨Update toppage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,93 @@
 import Image from "next/image";
+import Header from "@/components/layouts/Header";
+import SnsIcons from "@/components/ui/SnsIcons";
+import WorkCard from "@/components/ui/WorkCard";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <>
+      <Header />
+
+      <section>
+        <div className="max-w-(--content-width) mx-auto px-4">
+          <div className="py-20 md:flex md:gap-10">
+            <div datatype="mv-text">
+              <p>
+                I'm <strong>Takahiro Okada</strong>, a Web Engineer based in
+                Japan.
+              </p>
+              <p className="mt-4">
+                I've worked at a production company building e-commerce sites
+                with Shopify, and served as a Web Engineer at a business
+                company.
+              </p>
+              <p className="mt-4">
+                From February 2026, I'll be studying Computer Science at Lincoln
+                University in New Zealand while working as an IT Engineer.
+              </p>
+              <p className="mt-4">
+                I'm a proud father of three boys in a family of five. I also
+                love traveling.
+              </p>
+            </div>
+            <div className="mt-10 md:mt-0 md:flex-shrink-0 rounded-lg">
+              <Image
+                src="/images/myself.jpg"
+                alt="Takahiro Okada"
+                width={307}
+                height={230}
+                className="rounded-lg w-full object-cover"
+              />
+              <div className="mt-4">
+                <SnsIcons />
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+      </section>
+
+      <section className="mt-16">
+        <div className="max-w-(--content-width) mx-auto px-4">
+          <h2 className="text-4xl md:text-6xl font-just-me-again-down-here">
+            My Activity
+          </h2>
+          <p className="mt-4">Coming soon...</p>
         </div>
-      </main>
-    </div>
+      </section>
+
+      {/* Works */}
+      <section className="mt-16 mb-20 bg-[#f7fafc] py-20 relative">
+        <div className="absolute top-0 left-[50%] transform -translate-x-1/2 -translate-y-1/2">
+          <h2 className="text-4xl md:text-6xl font-just-me-again-down-here">
+            My Works
+          </h2>
+        </div>
+        <div className="max-w-(--content-width) mx-auto px-4">
+          <ul className="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+            {Array.from({ length: 3 }).map((_, index) => (
+              <WorkCard
+                key={index}
+                imageSrc="/images/sample.jpg"
+                alt={`Work ${index + 1}`}
+                title={"Work Title 1Work Title 1Work Title 1Work Title 1"}
+                tag="#personal"
+                techs={["Next.js", "TypeScript", "Tailwind", "Tailwind", "Tailwind"]}
+                href="#"
+              />
+            ))}
+          </ul>
+
+          <div className="mt-8 text-center">
+            <Link
+              href="/"
+              className="px-6 py-2 bg-[#f7fafc] border border-[#e2e8f0] rounded-lg hover:bg-[#e2e8f0] transition-colors"
+            >
+              View All Works
+            </Link>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
