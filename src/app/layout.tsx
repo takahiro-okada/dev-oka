39ebb043e35f3557acf13f56e2295c7af148088a
takahiro-okada <okahanako09875@gmail.com>
2026-01-07 10:38:43 +0900

    ✨ Update fonts & styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Just_Me_Again_Down_Here } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+// English font
+const justMeAgainDownHere = Just_Me_Again_Down_Here({
+  variable: "--font-just-me-again-down-here",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+  weight: "400"
+}); 
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,11 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${justMeAgainDownHere.variable} antialiased`}>{children}</body>
     </html>
   );
 }
